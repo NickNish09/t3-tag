13fc232c8493503d66d75a4d56a6c5854ab2ac95
Nicholas <nicholasmarques@cjr.org.br>
2018-06-04 02:50:45 -0300

    tentando regex

--- src/Professor.cpp
+++ src/Professor.cpp
@@ -51,14 +51,18 @@ void Professor::readProfessors(FILE * file)
   ssize_t read;
   Professor p;
   int i=0;
+  int j=0;
   int escolas_pretendidadas[5];
+  string escola_aux;
+  string linha;
 
   Professor professores[100];
-  regex habilitacao("d");
+  regex habilitacao(".*");
 
   file = fopen("professores.txt", "r");
   if (file){
     while ((read = getline(&line, &len, file)) != -1) {
+      linha = line;
       p.setNumeroProfessor(i+1);
       if(i<9){
         p.setNumeroHabilitacoes(line[5] - '0');
@@ -66,15 +70,23 @@ void Professor::readProfessors(FILE * file)
       }else{
         p.setNumeroHabilitacoes(line[6] - '0');
       }
+      for(j=0;j<5;j++){
+        escolas_pretendidadas[j] = j;
+      }
+      p.setEscolasPretendidas(escolas_pretendidadas);
+
       professores[i] = p;
       i++;
+      cmatch cm;
+      regex_match(line,cm,habilitacao,regex_constants::match_default);
+      printf("%s\n",cm[0]);
       //printf("%s", line)
     }
     setProfessores(professores);
 
     Professor::professores[99].setNumeroHabilitacoes(3);
     for(i=0;i<100;i++){
-      printf("Numero do Professor: %d\nHabilitaçoes: %d\n",Professor::professores[i].getNumeroProfessor(),Professor::professores[i].getNumeroHabilitacoes());
+      //printf("Numero do Professor: %d\nHabilitaçoes: %d\nPrimeira: %d\nSegunda: %d\nTerceira: %d\nQuarta: %d\nQuinta: %d\n",Professor::professores[i].getNumeroProfessor(),Professor::professores[i].getNumeroHabilitacoes(),Professor::professores[i].getEscolasPretendidas()[0],Professor::professores[i].getEscolasPretendidas()[1],Professor::professores[i].getEscolasPretendidas()[2],Professor::professores[i].getEscolasPretendidas()[3],Professor::professores[i].getEscolasPretendidas()[4]);
     }
   }
 }
